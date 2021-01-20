180e4057ae7c7ab2d488ac2b19a8c956754efadb
RowdyKGZ <RowdyKG@gmail.com>
2021-01-21 03:26:03 +0600

    Добавил ReadMe

--- webparser/models.py
+++ webparser/models.py
@@ -5,6 +5,7 @@ from django.db import models
 
 class TargetUrl(models.Model):
     url = models.CharField(max_length=100)
+    time_work = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.url
